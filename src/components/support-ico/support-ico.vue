3b0db3a474bf6fcc42f41e86a2ce7ea02947b448
Your Name <you@example.com>
2018-12-20 12:55:24 +0800

    项目初始化

---
+++ src/components/support-ico/support-ico.vue
@@ -0,0 +1,11 @@
+<template>
+  <span class="support-ico" :class="iconCls"></span>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ 项目笔记心得/ESlink.md
@@ -0,0 +1,7 @@
+**函数格式化缩进的时候忽略做空格 以免报错**
+
+rules:{
+
+  'space-before-function-paren': 0,
+    'indent': 0
+}
